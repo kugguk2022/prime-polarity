26817a875cb083a5a33fc5a3e8772b3d106621c4
zolton farkas <kugguk2022@gmail.com>
2025-08-22 19:08:11 +0100

    Update __init__.py

--- src/prime_polarity/__init__.py
+++ src/prime_polarity/__init__.py
@@ -1,3 +1,9 @@
 
 __all__ = ["generators", "transforms", "sieves", "metrics"]
 __version__ = "0.1.0"
+# src/prime_polarity/__main__.py
+from .cli import main
+
+if __name__ == "__main__":
+    main()
+
